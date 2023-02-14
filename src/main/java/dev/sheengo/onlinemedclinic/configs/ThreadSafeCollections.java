01bae296ccf8e4e886020f392f968edd6702f7a0
asror <asroryuldashev57@gmail.com>
2023-02-14 19:05:54 +0500

    fixed

---
+++ src/main/java/dev/sheengo/onlinemedclinic/configs/ThreadSafeCollections.java
@@ -0,0 +1,9 @@
+package dev.sheengo.onlinemedclinic.configs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ThreadSafeCollections {
+    List<Integer> id = Collections.synchronizedList(new ArrayList<>());
+}
--- src/main/java/dev/sheengo/onlinemedclinic/services/UserService.java
+++ src/main/java/dev/sheengo/onlinemedclinic/services/UserService.java
@@ -16,65 +16,65 @@ public class UserService extends Service<User>{
 
     @Override
     public HttpServletRequest service(HttpServletRequest request) {
-        String email = req.getParameter("email");
-        String password = req.getParameter("password");
-
-        User user = UserDAO.getInstance().get(email);
-
-        try {
-            HttpSession session = req.getSession();
-            session.setAttribute("firstname", user.getFullName());
-            session.setAttribute("id", user.getId());
-            session.setAttribute("role", user.getRole());
-
-            Cookie firstName = new Cookie("firstName", user.getFullName());
-            Cookie id = new Cookie("id", String.valueOf(user.getId()));
-            Cookie role = new Cookie("role", user.getRole());
-
-            firstName.setPath("/");
-            id.setPath("/");
-            firstName.setMaxAge(5 * 60 * 60);
-            id.setMaxAge(5 * 60 * 60);
-            role.setPath("/");
-            role.setMaxAge(5 * 60 * 60);
-
-            String url = req.getParameter("next");
-
-            resp.addCookie(id);
-            resp.addCookie(firstName);
-            resp.addCookie(role);
-
-            if (Objects.nonNull(url) && !url.isBlank()) {
-                resp.sendRedirect(url);
-            }
-            else if (user.getPassword().equals(password)) {
-                resp.sendRedirect( (user.getRole().equals("Admin")) ? "/adminPage" : "/showBooks");
-            } else
-                throw new Exception();
-        } catch (Exception e) {
-            e.printStackTrace();
-            RequestDispatcher dispatcher = req.getRequestDispatcher("/auth/error.jsp");
-            dispatcher.forward(req, resp);
-        }
+//        String email = req.getParameter("email");
+//        String password = req.getParameter("password");
+//
+//        User user = UserDAO.getInstance().get(email);
+//
+//        try {
+//            HttpSession session = req.getSession();
+//            session.setAttribute("firstname", user.getFullName());
+//            session.setAttribute("id", user.getId());
+//            session.setAttribute("role", user.getRole());
+//
+//            Cookie firstName = new Cookie("firstName", user.getFullName());
+//            Cookie id = new Cookie("id", String.valueOf(user.getId()));
+//            Cookie role = new Cookie("role", user.getRole());
+//
+//            firstName.setPath("/");
+//            id.setPath("/");
+//            firstName.setMaxAge(5 * 60 * 60);
+//            id.setMaxAge(5 * 60 * 60);
+//            role.setPath("/");
+//            role.setMaxAge(5 * 60 * 60);
+//
+//            String url = req.getParameter("next");
+//
+//            resp.addCookie(id);
+//            resp.addCookie(firstName);
+//            resp.addCookie(role);
+//
+//            if (Objects.nonNull(url) && !url.isBlank()) {
+//                resp.sendRedirect(url);
+//            }
+//            else if (user.getPassword().equals(password)) {
+//                resp.sendRedirect( (user.getRole().equals("Admin")) ? "/adminPage" : "/showBooks");
+//            } else
+//                throw new Exception();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            RequestDispatcher dispatcher = req.getRequestDispatcher("/auth/error.jsp");
+//            dispatcher.forward(req, resp);
+//        }
         return null;
     }
 
     @Override
     User save(User user) {
-        String firstName = request.getParameter("firstName");
-        String lastName = request.getParameter("lastName");
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-
-        UserDAO.getInstance().save(User.builder()
-                .fullName(firstName + " " + lastName)
-                .email(email)
-                .password(password)
-                .build());
-
-        request.setAttribute("email", email);
-        request.setAttribute("password", password);
-        request.getRequestDispatcher("/auth/index.jsp").forward(request, response);
+//        String firstName = request.getParameter("firstName");
+//        String lastName = request.getParameter("lastName");
+//        String email = request.getParameter("email");
+//        String password = request.getParameter("password");
+//
+//        UserDAO.getInstance().save(User.builder()
+//                .fullName(firstName + " " + lastName)
+//                .email(email)
+//                .password(password)
+//                .build());
+//
+//        request.setAttribute("email", email);
+//        request.setAttribute("password", password);
+//        request.getRequestDispatcher("/auth/index.jsp").forward(request, response);
         return null;
     }
 
