e3c44a1022831473d7081ab0ea860aeae691d063
asror <asroryuldashev57@gmail.com>
2023-02-16 18:35:51 +0500

    fixed

--- src/main/java/dev/sheengo/onlinemedclinic/controller/doctor/DoctorMainServlet.java
+++ src/main/java/dev/sheengo/onlinemedclinic/controller/doctor/DoctorMainServlet.java
@@ -1,6 +1,7 @@
 package dev.sheengo.onlinemedclinic.controller.doctor;
 
 import dev.sheengo.onlinemedclinic.dao.DoctorDAO;
+import dev.sheengo.onlinemedclinic.dao.UserDAO;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -13,8 +14,8 @@ public class DoctorMainServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         DoctorDAO doctorDAO = DoctorDAO.getInstance();
-        request.setAttribute("user", doctorDAO.findUserByUserId(1)); // 1 ni orniga sessiondagi user id bervoriladi
-        request.setAttribute("doctor", doctorDAO.findDoctorById(1)); // 1 ni orniga sessiondagi doctor id bervoriladi
+        request.setAttribute("user", UserDAO.getInstance().get(1)); // 1 ni orniga sessiondagi user id bervoriladi
+        request.setAttribute("doctor", doctorDAO.get(1)); // 1 ni orniga sessiondagi doctor id bervoriladi
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/views/doctorPages/main.jsp");
         dispatcher.forward(request, response);
--- src/main/java/dev/sheengo/onlinemedclinic/controller/doctor/DoctorProfileServlet.java
+++ src/main/java/dev/sheengo/onlinemedclinic/controller/doctor/DoctorProfileServlet.java
@@ -1,6 +1,7 @@
 package dev.sheengo.onlinemedclinic.controller.doctor;
 
 import dev.sheengo.onlinemedclinic.dao.DoctorDAO;
+import dev.sheengo.onlinemedclinic.dao.UserDAO;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -12,10 +13,10 @@ public class DoctorProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        DoctorDAO doctorDAO = DoctorDAO.getInstance();
-        request.setAttribute("user", doctorDAO.findUserByUserId(1)); // 1 ni orniga sessiondagi user id bervoriladi
-        request.setAttribute("doctor", doctorDAO.findDoctorById(1)); // 1 ni orniga sessiondagi doctor id bervoriladi
-        request.setAttribute("specialization", doctorDAO.findSpecializationSpecializationId(1)); // 1 ni orniga sessiondagi doctorni id bervoriladi
+        UserDAO userDAO = UserDAO.getInstance();
+        request.setAttribute("user", userDAO.get(1)); // 1 ni orniga sessiondagi user id bervoriladi
+        request.setAttribute("doctor", userDAO.get(1)); // 1 ni orniga sessiondagi doctor id bervoriladi
+        request.setAttribute("specialization", DoctorDAO.getInstance().findSpecializationSpecializationId(1)); // 1 ni orniga sessiondagi doctorni id bervoriladi
         RequestDispatcher dispatcher = request.getRequestDispatcher("/views/doctorPages/profile.jsp");
         dispatcher.forward(request, response);
     }
