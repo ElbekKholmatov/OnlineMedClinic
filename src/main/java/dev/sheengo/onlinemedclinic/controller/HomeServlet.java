dc010a58119a03336809fe66eb4a6cd6badfcfd7
asror <asroryuldashev57@gmail.com>
2023-02-16 23:33:29 +0500

    fixed

--- src/main/java/dev/sheengo/onlinemedclinic/controller/HomeServlet.java
+++ src/main/java/dev/sheengo/onlinemedclinic/controller/HomeServlet.java
@@ -1,13 +1,10 @@
 package dev.sheengo.onlinemedclinic.controller;
 
-import dev.sheengo.onlinemedclinic.services.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
 
 import java.io.IOException;
 
